727f480eb52f436847816cbdcbc7a03b1be01437
Jessica Smith <jess@mintopia.net>
2018-12-28 13:09:31 +0100

    Might break everything, but first attempt

--- PointSaberPlugin/Patch.cs
+++ PointSaberPlugin/Patch.cs
@@ -9,15 +9,7 @@ class LocalLeaderboardsModel_AddScore_Patch
 {
     public static void Postfix(LocalLeaderboardsModel __instance)
     {
-        try
-        {
-            __instance.SaveData();
-        }
-        catch
-        {
-
-        }
-
+ 
         try
         {
             PostScoreBehavior.PostScores();
